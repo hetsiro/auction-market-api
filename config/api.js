3969e09160d9b80da0c1530f33bd1c07ee6e0f6b
Cristobal <FHCristobal@hotmail.com>
2025-05-31 23:26:49 -0400

    build: changed production API URL

--- config/api.js
+++ config/api.js
@@ -1,3 +1,5 @@
+// Production API
+exports.API_URL = 'https://auction-market-api.onrender.com';
 
 // Test API
-exports.API_URL = 'http://localhost:5173';
+// exports.API_URL = 'http://localhost:5173';
